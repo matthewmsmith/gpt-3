148f00044e8063f4aa75bd3c6a8fea500a38b933
Matthew Smith <Matthewmsmith1992@gmail.com>
2022-11-25 06:45:00 -0500

    What GPT3 is component setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-import {Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
-import {CTA, Brand, Navbar} from './components'
-import './App.css'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
-const App = () => {
-  return (
-    <div className='App'>
-        <div className="gradient__bg">
-            <Navbar />
-            <Header />
-        </div>  
-        <Brand />
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Blog />
-        <Footer />
+import './App.css';
 
-
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import './whatGPT3.css'
+import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
-  )
-}
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container"></div>
+    </div>
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
