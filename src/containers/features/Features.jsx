d06e549e010a593f1eb53f880b267c6eddc9fae9
Matthew Smith <Matthewmsmith1992@gmail.com>
2022-12-10 06:43:51 -0500

    Possibility section complete

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
-
+import { Feature } from '../../components'
 import './features.css'
+
+const featuresData = [ 
+  {
+    title: 'Improving end distrusts instantly ',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Become the tended active ',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Really boy law county ',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+]
 
 const Features = () => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step Into the Future Today.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={index.title + index}/>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import Feature from '../../components/feature/Feature'
 import "./whatGPT3.css";
 
+const featureData = [ 
+
+ {
+  title:'Chatbot', 
+  text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. '
+ },
+ {
+  title: 'Knowledgebase',
+  text:'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments '
+ }, 
+ {
+  title:'Education',
+  text:'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments '
+ }
+
+]
 const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      <Feature title='What is GPT-3'
+    text = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -15,9 +32,9 @@ const WhatGPT3 = () => {
         <p>Explore the library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature title='Chatbot' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
-        <Feature title='Knowledgebase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
-        <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
+      {featureData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ))}
       </div>
     </div>
   );
